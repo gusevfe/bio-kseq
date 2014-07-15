ab0f094ee3ce41ecda9d00de61c372e5865a2241
Fedor Gusev <gusevfe@gmail.com>
2014-07-15 15:22:57 +0400

    Add timing script.

---
+++ timing.rb
@@ -0,0 +1,33 @@
+require 'bio'
+require 'bio/kseq'
+require 'benchmark'
+require 'tempfile'
+
+tmp = Tempfile.new 'bio-kseq'
+
+1.upto(100000) do |n|
+  tmp.puts "@" + n.to_s + " " + "comment"
+  tmp.puts "AAAAAAAAAAAAAAAAAAAAAAA"
+  tmp.puts "+" + n.to_s + " " + "comment"
+  tmp.puts "EEEEEEEEEEEEEEEEEEEEEEE"
+end
+
+tmp.close
+
+Benchmark.bm do |x|
+  x.report("BioRuby") do 
+    Bio::FlatFile.open(Bio::Fastq, tmp.path).each_entry { |e| }
+  end
+
+  x.report("Bio::Faster") do
+    fastq = Bio::Faster.new tmp.path
+    fastq.each_record do |sequence_header, sequence, quality|
+    end
+  end
+
+  x.report("Bio::Kseq") do 
+    kseq = Bio::Kseq.new tmp.path
+    while kseq.read! do
+    end
+  end
+end
