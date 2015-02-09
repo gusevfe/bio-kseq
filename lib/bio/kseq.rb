292e3db9d94c54a17bbabd6537272b9d24e3c990
Fedor Gusev <gusevfe@gmail.com>
2015-02-09 13:23:27 +0300

    Fix to_s method.

--- lib/bio/kseq.rb
+++ lib/bio/kseq.rb
@@ -2,21 +2,19 @@ require "seqtk_bindings"
 require "bio/kseq/version"
 
 module Bio
-  module SeqTK
-    class Kseq
-      def to_s
-        if qual.nil?
-          if comment.nil?
-            ">" + name + "\n" + seq
-          else
-            ">" + name + " " + comment + "\n" + seq
-          end
+  class Kseq
+    def to_s
+      if qual.nil?
+        if comment.nil?
+          ">" + name + "\n" + seq
         else
-          if comment.nil?
-            "@" + name + "\n" + seq + "\n+\n" + qual
-          else
-            "@" + name + " " + comment + "\n" + seq + "\n+\n" + qual
-          end
+          ">" + name + " " + comment + "\n" + seq
+        end
+      else
+        if comment.nil?
+          "@" + name + "\n" + seq + "\n+\n" + qual
+        else
+          "@" + name + " " + comment + "\n" + seq + "\n+\n" + qual
         end
       end
     end
